c27450188887e5e6dd421c79937df47ce627589c
Warfa Jibril <warfaj@gmail.com>
2025-02-18 18:32:47 -0500

    fixed jacob comments on 1

--- 1/train.py
+++ 1/train.py
@@ -2,39 +2,40 @@
 
 import numpy as np
 
+# Target function
 def f(x):
     return 10*x - 12
 
-vectorized_fx = np.vectorize(f)
+# Create data (randomized and whitened)
+input = np.arange(100000)
+np.random.shuffle(input) 
+input.mean()
+input = (input-input.mean())/input.std()
+targets = f(input)
 
-x = np.arange(100)
-data = np.stack([x,vectorized_fx(x)])
 
+# Initialize weights and hyperparameters
 w = np.array([0.1, 0.1])
-
 iters = 0
-max_iters = 1000
-
-batch_size = 1
+max_iters = 100
+batch_size = 2
 learning_rate = 0.0001
 
-while iters <= max_iters:
-    for i in range(int(len(data[0])/batch_size)):
-        gradient = np.zeros(2, dtype=np.float64)
-        loss = 0
-        for j in range(batch_size):
-            x = data[0][(i*batch_size) + j]
-            t = data[1][(i*batch_size) + j]
-            output = w[1] * x + w[0]
-            loss += 0.5*(t - output)**2 /batch_size
-            gradient[0] += learning_rate * (t - output) / batch_size
-            gradient[1] += learning_rate * (t - output) * x/batch_size
-        print("calculating batch")
-        print("loss: ", loss)
-        print("gradient: ", gradient)
-        print("prev weights: ", w)
-        w +=  gradient
-        print("new weights: ",w)
+# Process each batch 
+for i in range(0, input.shape[0], batch_size):
+    if iters > max_iters: 
+        break
+    x = input[i : i+batch_size ]
+    target = targets[i: i+batch_size]
+    output = w[1]*x+w[0]
+    loss = 0.5*(target - output)**2
+    gradient_w0 = (target-output)
+    gradient_w1 = (target - output)* x
+    print("loss: ", np.mean(loss))
+    print("prev weights: ", w)
+    w[0] += learning_rate * np.mean(gradient_w0)
+    w[1] += learning_rate * np.mean(gradient_w1)
+    print("new weights: ",w)
     iters += 1
 
 print(w)
